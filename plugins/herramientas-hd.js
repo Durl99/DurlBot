66dc5380b343a2f24a782cf431b746a9d84d946b
DEIMOS-666-T <92341694+DEIMOS-666-T@users.noreply.github.com>
2023-09-17 10:26:41 -0600

    Add files via upload

---
+++ plugins/herramientas-hd.js
@@ -0,0 +1,46 @@
+import FormData from "form-data";
+import Jimp from "jimp";
+const handler = async (m, {conn, usedPrefix, command}) => {
+ try {    
+  let q = m.quoted ? m.quoted : m;
+  let mime = (q.msg || q).mimetype || q.mediaType || "";
+  if (!mime) throw `*[❗] ENVIE UNA IMAGEN O RESPONDA A UNA IMAGEN CON EL COMANDO ${usedPrefix + command}*`;
+  if (!/image\/(jpe?g|png)/.test(mime)) throw `*[❗] EL FORMATO DEL ARCHIVO (${mime}) NO ES COMPATIBLE, ENVÍA O RESPONDE A UNA FOTO*`;
+  m.reply("*[❗] PROCESANDO IMAGEN...*");
+  let img = await q.download?.();
+  let pr = await remini(img, "enhance");
+  conn.sendMessage(m.chat, {image: pr}, {quoted: m});
+ } catch {
+  throw "*[❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙴 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾*";
+ }
+};
+handler.help = ["remini", "hd", "enhance"];
+handler.tags = ["ai", "tools"];
+handler.command = ["remini", "hd", "enhance"];
+export default handler;
+
+async function remini(imageData, operation) {
+  return new Promise(async (resolve, reject) => {
+    const availableOperations = ["enhance", "recolor", "dehaze"];
+    if (availableOperations.includes(operation)) {
+      operation = operation;
+    } else {
+      operation = availableOperations[0];
+    }
+    const baseUrl = "https://inferenceengine.vyro.ai/" + operation + ".vyro";
+    const formData = new FormData();
+    formData.append("image", Buffer.from(imageData), {filename: "enhance_image_body.jpg", contentType: "image/jpeg"});
+    formData.append("model_version", 1, {"Content-Transfer-Encoding": "binary", contentType: "multipart/form-data; charset=utf-8"});
+    formData.submit({url: baseUrl, host: "inferenceengine.vyro.ai", path: "/" + operation, protocol: "https:", headers: {"User-Agent": "okhttp/4.9.3", Connection: "Keep-Alive", "Accept-Encoding": "gzip"}},
+      function (err, res) {
+        if (err) reject(err);
+        const chunks = [];
+        res.on("data", function (chunk) {chunks.push(chunk)});
+        res.on("end", function () {resolve(Buffer.concat(chunks))});
+        res.on("error", function (err) {
+        reject(err);
+        });
+      },
+    );
+  });
+}
